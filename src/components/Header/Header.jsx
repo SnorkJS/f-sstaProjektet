42232b6955bfa25c3139fdc92d15c76c04929575
SnorkJS <83785234+SnorkJS@users.noreply.github.com>
2023-05-17 14:20:59 +0200

    yourmom

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,6 @@ import React from "react";
 
 export default function Header() {
     return(
-            <img width="100%" aria-label="picture of motorcycle" src="/KTM.jpg" />
+            <img fetchpriority="high" width="100%" aria-label="picture of motorcycle" src="/KTM.jpg" />
     )
 }
