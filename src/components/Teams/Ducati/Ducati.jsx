87ca56faed556a61a41864a38b05cefd9a4fb838
SnorkJS <83785234+SnorkJS@users.noreply.github.com>
2023-05-17 18:32:58 +0200

    yourmom

--- src/components/Teams/Ducati/Ducati.jsx
+++ src/components/Teams/Ducati/Ducati.jsx
@@ -5,6 +5,6 @@ import EB23 from "../../../img/EB23.png";
 
 export default function Aprilia() {
     return(
-        <RiderCard name="FB63" avatar={FB63} /* || */ name2="EB23" avatar2={EB23}/>
+        <RiderCard style={{ backgroundImage: 'radial-gradient(circle, #b00e0a, #9f090b, #8e050a, #7d0209, #6d0106, #610012, #540017, #46001a, #340720, #210b1f, #100716, #000001)' }} name="FB63" avatar={FB63} /* || */ name2="EB23" avatar2={EB23}/>
     )
 }
