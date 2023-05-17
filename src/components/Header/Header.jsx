d1627f971dc5fcd24d71c4eb02b9faf931d69f68
SnorkJS <83785234+SnorkJS@users.noreply.github.com>
2023-05-17 13:00:44 +0200

    yourmom
    Next step, roast the potatoes.
    Aye your mom ye!!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import "./Header.css";
-import MM93 from "../../img/MM93.jpg";
-import KTM from "../../img/KTM.jpg";
+
 
 export default function Header() {
     return(
-        <div>
-            <link rel="preload" as="image" href="/static/media/KTM.dbca0db8aa61f0b9e671.jpg"></link>
-            <img aria-label="picture of motorcycle" className="header-img" src={KTM}/>
-        </div>
+            <img width="100%" aria-label="picture of motorcycle" src="/KTM.jpg" />
     )
 }
--- src/components/NewsCards/Card.jsx
+++ src/components/NewsCards/Card.jsx
@@ -4,7 +4,7 @@ import "./Card.css";
 
 export default function Card(props) {
     return(
-        <div id="card">
+        <div className="card">
             <span className="card-source">motogp.com</span>
             <img alt="news story" width="300" height="200" className="card-img" src={props.img}/>
             <h2 className="card-name">
